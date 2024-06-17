28deafffc512c31877001487b0f0910fb733ba26
Om Rajpal <omrajpal.exe@gmail.com>
2024-06-17 11:44:15 +0530

    cleaned up the code and made a separate file form forms schema

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,49 +1,18 @@
-const mongoose = require("mongoose")
-
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    console.log('connected to mongo');
-    
-}).catch((err)=>{
-    console.log(err);
-    
-});
+const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: (true, "Please enter the username")
-    },
-    password: {
-        type: String,
-        required: (true, "Please enter your password")
-    }
+  username: {
+    type: String,
+    required: (true, "Please enter the username"),
+  },
+  password: {
+    type: String,
+    required: (true, "Please enter your password"),
+  },
+});
 
-})
-
-const formsSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: (true, "Please enter your name")
-    },
-    email: {
-        type: String,
-        required: (true, "Please enter your email")
-    },
-    phoneNumber: {
-        type: String,
-        required: (true, "Please enter your phone number")
-    },
-    departments: {
-        type: String,
-        enum: ['Tech', 'Design', 'Management'],
-        required: true
-    }
-})
-
-const Admin = mongoose.model("admin", adminSchema)
-const FormsData = mongoose.model("response", formsSchema)
+const Admin = mongoose.model("admin", adminSchema);
 
 module.exports = {
-    Admin,
-    FormsData
-}
+  Admin,
+};
