51d068265832342d032d2680ef3c00fafbb1efde
Parth Katiyar <parthdev0101@gmail.com>
2024-06-17 11:03:56 +0530

    Update

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Get the directory name for ES6 modules
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
