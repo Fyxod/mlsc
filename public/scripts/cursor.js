95a8688234d4f91cc2e5ca5fc6236e4a51f54ed4
Om Rajpal <omrajpal.exe@gmail.com>
2024-06-21 21:13:08 +0530

    UI changes in forms

--- public/scripts/cursor.js
+++ public/scripts/cursor.js
@@ -1,5 +1,9 @@
 var circle = document.querySelector("#circle");
+
+// Check if the element exists before adding event listeners
 var box1 = document.querySelector(".swiper");
+var box2 = document.querySelector("#forms");
+
 document.body.addEventListener("mousemove", function (dets) {
   gsap.to(circle, {
     x: dets.clientX,
@@ -8,23 +12,47 @@ document.body.addEventListener("mousemove", function (dets) {
   });
 });
 
-box1.addEventListener("mouseenter", function () {
-  circle.innerHTML = `<img src="../assets/images/horizontal.png" alt="" width="13px">`;
-  gsap.to(circle, {
-    duration: 0.6,
-    scale: 2.5,
-    color: "#0e0e0e",
-    opacity: 1,
-    backgroundColor: "#0077ff",
+if (box1) {
+  box1.addEventListener("mouseenter", function () {
+    circle.innerHTML = `<img src="../assets/images/horizontal.png" alt="" width="13px">`;
+    gsap.to(circle, {
+      duration: 0.6,
+      scale: 2.5,
+      color: "#0e0e0e",
+      opacity: 1,
+      backgroundColor: "#0077ff",
+    });
   });
-});
 
-box1.addEventListener("mouseleave", function () {
-  circle.innerHTML = "";
-  gsap.to(circle, {
-    duration: 0.6,
-    scale: 1,
-    backgroundColor: "",
-    opacity: 1,
+  box1.addEventListener("mouseleave", function () {
+    circle.innerHTML = "";
+    gsap.to(circle, {
+      duration: 0.6,
+      scale: 1,
+      backgroundColor: "",
+      opacity: 1,
+    });
   });
-});
+}
+
+if (box2) {
+  box2.addEventListener("mouseenter", function () {
+    gsap.to(circle, {
+      duration: 0.6,
+      scale: 2.5,
+      color: "#0e0e0e",
+      opacity: 1,
+      backgroundColor: "#0077ff",
+    });
+  });
+
+  box2.addEventListener("mouseleave", function () {
+    circle.innerHTML = "";
+    gsap.to(circle, {
+      duration: 0.6,
+      scale: 1,
+      backgroundColor: "",
+      opacity: 1,
+    });
+  });
+}
